8914350b229c896d2c9589b0ce5ad3e5098273c4
Varun Naik <naik.varun@gmail.com>
2024-06-21 08:15:01 -0500

    cached query

--- src/components/form-button.tsx
+++ src/components/form-button.tsx
@@ -12,7 +12,6 @@ interface FormButtonProps {
 
 export default function FormButton({children, className}: FormButtonProps) {
   const {pending} = useFormStatus()
-  console.log(`pending ${pending} pending`)
   return <Button type="submit" className={cn("w-full", className)} disabled={pending} aria-disabled={pending} >
     {
       pending && <Loader className="animate-spin mr-2" />
--- src/db/queries/comment-query.ts
+++ src/db/queries/comment-query.ts
@@ -1,11 +1,12 @@
 import {Comment, User} from "@prisma/client";
 import {db} from "@/db";
+import { cache } from "react";
 
 export type CommentWithAuthor = Comment & {
   user: Pick<User, 'name' | 'image'>;
 }
 
-export const fetchCommentForPost = (postId: string): Promise<CommentWithAuthor[]> => {
+export const fetchCommentForPost = cache(  (postId: string): Promise<CommentWithAuthor[]> => {
   console.log('Fetching comment for post', postId);
   return db.comment.findMany({
     where: {postId},
@@ -19,3 +20,4 @@ export const fetchCommentForPost = (postId: string): Promise<CommentWithAuthor[]
     }
   })
 }
+)
